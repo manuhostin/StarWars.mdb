5ae5dd1b3fe45230d0fe9b8888f139af7bed8f22
isabellirosa <isabelli.ifc@gmail.com>
2024-11-18 16:59:06 -0300

    continuando

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,5 @@
+
 <!-- <script setup>
-  import { ref, onMounted } from 'vue';
-  import axios from 'axios';
-
-  const moviesGenres = ref([]);
-  const TVGenres = ref([]);
-
-  onMounted(async () => {
-    let response = await axios.get(
-      'https://api.themoviedb.org/3/genre/movie/list?language=pt-BR',
-      {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzJmMzZlNGVkMzljZjJiZDJhMjI5ZjNhM2JkMjZhOCIsIm5iZiI6MTczMTM1MzY5NS41NDM4NjYsInN1YiI6IjY3MzI1YTNmZjNlYWZjNTIwMWZkNTYzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4TLLWxX_XHj_6dIiWqt04Ym8pFuglxjCbFsg-hzWVtQ`,
-        },
-      },
-    );
-    moviesGenres.value = response.data.genres;
-    response = await axios.get(
-      'https://api.themoviedb.org/3/genre/tv/list?language=pt-BR',
-      {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzJmMzZlNGVkMzljZjJiZDJhMjI5ZjNhM2JkMjZhOCIsIm5iZiI6MTczMTM1MzY5NS41NDM4NjYsInN1YiI6IjY3MzI1YTNmZjNlYWZjNTIwMWZkNTYzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4TLLWxX_XHj_6dIiWqt04Ym8pFuglxjCbFsg-hzWVtQ`,
-        },
-      },
-    );
-    TVGenres.value = response.data.genres;
-  });
-</script> -->
-<script setup>
   import { ref, onMounted } from 'vue';
   import api from './plugins/axios';
 
@@ -55,4 +28,42 @@
       {{ genre.name }}
     </li>
   </ul>
+</template> -->
+
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+
+<style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer 9c2f36e4ed39cf2bd2a229f3a3bd26a8`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzJmMzZlNGVkMzljZjJiZDJhMjI5ZjNhM2JkMjZhOCIsIm5iZiI6MTczMTk1NDc5Mi43NDgyMzQzLCJzdWIiOiI2NzMyNWEzZmYzZWFmYzUyMDFmZDU2MzIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1Tic9W28Tk_YeY_gXiNpK3L1ztF-5_NTnFnusvL1g7w`,
   },
 });
 
